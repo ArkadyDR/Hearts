dce44bd980ece3ce69f07b6528f20586638f1c78
Jonathan Gostelow <jonathan.gostelow@gmail.com>
2016-04-12 23:20:23 -0500

    Wrap game/hands/{pid} response into a hash to be more like a RESTful show endpoint

--- src/endpoints.js
+++ src/endpoints.js
@@ -19,7 +19,7 @@ export default function configureEndpoints(server) {
 
       handler: (request, reply) => {
         const hand = game.getHand(request.params.pid);
-        reply(hand);
+        reply({pid: request.params.pid, hand: hand});
       },
     },
   });
